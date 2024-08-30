afe79d2a83bf7fce302b84455a5ef06416554ae1
ryanmv2304 <ST10365052@vcconnect.edu.za>
2024-08-30 10:56:58 +0200

    Add project files.

---
+++ cldv2APP/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+using Azure;
+using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+using Azure.Storage.Files.Shares;
+using Azure.Storage.Queues;
+using cldv2APP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace cldv2APP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile image)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=st10365052demostorage;AccountKey=5fTakvNfnJG3Gm7ndmyFD1gno7X9MsKJbKiuSLZNYxdz6VPzTzH28MpyK/H8u/mZbRQ74C1OOAyO+AStQ/yUkg==;EndpointSuffix=core.windows.net";
+            string containerName = "st10365052container";
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+
+            string blobName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            using (var stream = image.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadPDF(IFormFile pdf)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=st10365052demostorage;AccountKey=5fTakvNfnJG3Gm7ndmyFD1gno7X9MsKJbKiuSLZNYxdz6VPzTzH28MpyK/H8u/mZbRQ74C1OOAyO+AStQ/yUkg==;EndpointSuffix=core.windows.net";
+            string shareName = "st10365052fileshare";
+            string directoryName = "pdfs";
+
+            ShareServiceClient shareServiceClient = new ShareServiceClient(connectionString);
+            ShareClient shareClient = shareServiceClient.GetShareClient(shareName);
+            await shareClient.CreateIfNotExistsAsync();
+
+            ShareDirectoryClient directoryClient = shareClient.GetDirectoryClient(directoryName);
+            await directoryClient.CreateIfNotExistsAsync();
+
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(pdf.FileName);
+            ShareFileClient fileClient = directoryClient.GetFileClient(fileName);
+
+            using (var stream = pdf.OpenReadStream())
+            {
+                await fileClient.CreateAsync(stream.Length);
+                await fileClient.UploadRangeAsync(new HttpRange(0, stream.Length), stream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessTransaction()
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=st10365052demostorage;AccountKey=5fTakvNfnJG3Gm7ndmyFD1gno7X9MsKJbKiuSLZNYxdz6VPzTzH28MpyK/H8u/mZbRQ74C1OOAyO+AStQ/yUkg==;EndpointSuffix=core.windows.net";
+            string queueName = "st10365052queue";
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+            await queueClient.CreateIfNotExistsAsync();
+
+            string message = "Order has been processed";
+            await queueClient.SendMessageAsync(message);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitUserDetails(string name, string email, string password, string city)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=st10365052demostorage;AccountKey=5fTakvNfnJG3Gm7ndmyFD1gno7X9MsKJbKiuSLZNYxdz6VPzTzH28MpyK/H8u/mZbRQ74C1OOAyO+AStQ/yUkg==;EndpointSuffix=core.windows.net";
+            string tableName = "userTable";
+
+            TableServiceClient tableServiceClient = new TableServiceClient(connectionString);
+            TableClient tableClient = tableServiceClient.GetTableClient(tableName);
+            await tableClient.CreateIfNotExistsAsync();
+
+            var customerEntity = new TableEntity("Customer", Guid.NewGuid().ToString())
+                {
+                    { "Name", name },
+                    { "Email", email },
+                    { "Password", password },
+                    { "City", city }
+                };
+
+            await tableClient.AddEntityAsync(customerEntity);
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
